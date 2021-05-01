9acebe9180e5ab260c7a131f5c276fbcab020d81
milangroshev <groshevmilan@gmail.com>
2021-05-02 01:41:02 +0200

    last

--- turtlebot2-service/roscore-map-server/run_example.sh
+++ turtlebot2-service/roscore-map-server/run_example.sh
@@ -3,6 +3,8 @@
 #Assemble docker image. 
 echo 'Running roscore docker image.'
 
+MAP="/opt/ros/kinetic/share/map_server/maze.yaml"
+
 sudo docker run \
         --hostname roscore-map-server \
         -it \
@@ -10,10 +12,11 @@ sudo docker run \
         --rm \
         -d \
         --net host\
-        -e ROS_MASTER_URI="http://192.168.55.99:11311" \
+        -e ROS_MASTER_URI="http://127.0.0.1:11311" \
+        -e TURTLEBOT_STAGE_MAP_FILE=$MAP \
         --add-host robot01:192.168.55.7 \
-        --add-host roscore-map-server:192.168.55.99 \
-        --add-host amcl:192.168.55.99 \
-        --add-host auto-nav:192.168.55.99 \
-        --add-host drivers:192.168.55.77 \
+        --add-host roscore-map-server:127.0.0.1 \
+        --add-host amcl:127.0.0.1 \
+        --add-host auto-nav:127.0.0.1 \
+        --add-host drivers:127.0.0.1 \
         roscore-map-server:latest
